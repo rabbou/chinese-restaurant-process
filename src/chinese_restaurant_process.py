4c867ac483ab1fb5a2e12d73809fa20f405911c0
jackpotrykus <jackpotrykus@gmail.com>
2021-03-13 15:20:47 -0600

    initial commit

---
+++ src/chinese_restaurant_process.py
@@ -0,0 +1,128 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+class ChineseRestaurantProcess:
+    """
+    Initializes and provides utilities to draw from Chinese Restuarant Process
+    """
+
+    def __init__(self, alpha):
+        """
+        Initialize a ChineseRestaurantProcess class with concentration parameter `alpha`
+        Higher choice of `alpha` leads to more tables for fixed number of customers
+
+        Attributes
+        ----------
+        alpha: float
+            concentration parameter
+        tables: dict
+            keys are table #s (ints), values are arrays, indicating which `n` resulted
+            in a customere sitting at that table
+        history: list
+            list of all previous states of the Process
+        n: int
+            number of states simulated, plus 1
+        """
+        self.alpha = alpha
+        self.tables = {}
+        self.history = []
+        self.n = 1
+
+    def __repr__(self):
+        df = self.to_pandas()
+        ntables = len(df)
+        return """Chinese Restaurant Process
+
+Parameters
+----------
+alpha = {}
+n     = {}
+
+Simulation results
+------------------
+Number of tables = {}
+Number of customers at each table:
+{}
+""".format(
+            self.alpha, self.n - 1, ntables, df
+        )
+
+    def to_pandas(self):
+        return pd.DataFrame.from_dict(
+            self.get_table_sizes(), orient="index", columns=["Number of Customers"]
+        )
+
+    def get_table_sizes(self):
+        """
+        Find out how many people are sitting at each table
+
+        Returns
+        -------
+        dict:
+            keys = table ids (int), values = # of customers at each table (int)
+        """
+        return {k: len(v) for k, v in self.tables.items()}
+
+    def get_wts(self):
+        """
+        Returns
+        -------
+        float:
+            probability of sitting at a new table
+        array:
+            probabilities of sitting at each of the existing tables, conditional on
+            _not_ sitting at a new table
+        """
+        table_sizes = np.array([len(v) for v in self.tables.values()])
+        return self.alpha / (self.n - 1 + self.alpha), table_sizes / np.sum(table_sizes)
+
+    def iter(self, niter):
+        """
+        Iterate the process `niter` times.
+
+        Parameters
+        ----------
+        niter: int
+            number of draws to make from the Process
+
+        Returns
+        -------
+        ChineseRestaurantProcess:
+            Returns `self`, so you can call it inline with assignment, i.e.
+        ```python
+        crt = ChineseRestaurantProcess(alpha=1).iter(100)
+        ```
+        """
+
+        def step(self):
+            """
+            Execute a single step in the Process
+            """
+            # Get probability of new table and normalized weights of existing tables
+            new, probs = self.get_wts()
+            if np.random.random() <= new:
+                # Add a new table to `self.tables`
+                self.tables[len(self.tables)] = np.array([self.n])
+            else:
+                # Append `self.n` to an existing table
+                sel = np.random.choice(np.arange(len(self.tables)), size=1, p=probs)[0]
+                self.tables[sel] = np.append(self.tables[sel], self.n)
+            # Increment `n` before exiting
+            self.n += 1
+
+        for ii in np.arange(niter):
+            # Preserve previous states of the Process in `self.history`
+            self.history.append(self.get_table_sizes())
+            step(self)
+
+        return self
+
+    def visualize(self):
+        fig = plt.figure((8, 5))
+
+
+if __name__ == "__main__":
+    crt = ChineseRestaurantProcess(alpha=2.5).iter(100)
+    print(crt)
